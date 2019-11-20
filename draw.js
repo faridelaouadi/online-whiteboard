6e5dd7766a2d016aea50bbd678eb734ff12a2775
Farid El-Aouadi <faridelaouadi@hotmail.co.uk>
2019-11-20 18:03:14 +0000

    adding undo button - harder than i thought

--- draw.js
+++ draw.js
@@ -7,6 +7,10 @@ document.addEventListener('DOMContentLoaded', () => {
     let points = [];
     let lines = [];
     let svg = null;
+    let actions = []; //this is a list of all the actions done by the user wrt drawing, point or line to help with undo function
+    let line_start = [] //array of lines indices where the user started drawing lines
+
+    let redo_stack = [];
 
     function render() {
 
@@ -15,21 +19,35 @@ document.addEventListener('DOMContentLoaded', () => {
                 .attr('height', 0.9*(window.innerHeight - $( '#top_bar' ).height()))
                 .attr('width', 0.99*window.innerWidth);
 
+        //when the mouse is down in the svg region and the mouse is not moving
         svg.on('mousedown', function() {
             draw = true;
-            const coords = d3.mouse(this);
-            draw_point(coords[0], coords[1], false);
+            const coords = d3.mouse(this); //get the coordinates of the mouse
+            actions.push("point");
+            draw_point(coords[0], coords[1], false); //draw the point
         });
+
 
         svg.on('mouseup', () =>{
-            draw = false;
+            draw = false; //if the mouse is up
         });
 
+        //if the user is moving the mouse
         svg.on('mousemove', function() {
+          //we need to track when a user just started to draw the line and when they finish.
+
             if (!draw)
-                return;
-            const coords = d3.mouse(this);
+                return; //not drawing, dont do anything
+            //the user is drawing and moving the mouse
+            if (actions[actions.length -1] !== "line"){
+              //they just started drawing the line
+              line_start.push(lines.length - 1) // get the index of lines where the line starts
+            }
+            const coords = d3.mouse(this); //get the mouse coordinates
+            actions.push("line");
             draw_point(coords[0], coords[1], true);
+            //draw a point that is connected to the previous point
+            //we have to do it like this because we cant edit the number of times the getMousePosition event fires
         });
 
         document.querySelector('#erase').onclick = () => {
@@ -39,7 +57,27 @@ document.addEventListener('DOMContentLoaded', () => {
                 lines[i].remove();
             points = [];
             lines = [];
+            //we cant just assign them empty lists,we must do the for loop to remove all the SVG objects we have created
             close_sidebar();
+        }
+
+        document.querySelector('#undo').onclick = () => {
+          let most_recent_action = actions[actions.length-1]; //retrive last action
+          if (most_recent_action === "point"){
+            points[points.length - 1].remove();
+            points.pop();
+            //remove the most recent point
+          }else{
+            //it is a line, we want to pop off from
+            for (let i = line_start[line_start.length - 1]; i < lines.length; i++){
+              lines[lines.length - 1].remove();
+              lines.pop();
+              actions.pop(); //remove last action done by the user
+            }
+            line_start.pop(); //the last stroke has now been removed
+          }
+
+
         }
 
     }
@@ -67,7 +105,7 @@ document.addEventListener('DOMContentLoaded', () => {
                          .attr('cx', x)
                          .attr('cy', y)
                          .attr('r', thickness)
-                         .style('fill', color);
+                         .style('fill', "red");
         points.push(point);
     }
 
